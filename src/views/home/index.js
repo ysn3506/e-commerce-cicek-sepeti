8cbb38ad2a028b5391df3036cc76f573ca4f4d89
Yasin ULUSOY <yasin.ulusoy@yahoo.com>
2023-03-12 12:55:16 +0300

    category update feature added

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -4,27 +4,45 @@ import Container from "../../components/container";
 import Button from "../../components/button";
 import "./style.scss";
 import CategoryIcon from "../../components/CategoryIcon";
+import { setSelectedCategory } from "../../storage/redux/actions";
 
 function Home() {
-  const { categories } = useSelector((state) => state);
+  const { categories, categoryId } = useSelector((state) => state);
 
-  const renderCategories = () => (
-    <>
-      <Button classes="category-button">Tüm Kategoriler</Button>
-      {categories.map((el) => (
-        <Button key={el.categoryId} classes="category-button">{el.categoryName}</Button>
-      ))}
-    </>
-  );
-    
-
-    
-
+  const changeCategory = (id) => {
+    const selectedCategory = categories.find((el) => el.categoryId === id);
+    const newCategoryId =
+      id === 0 || categoryId === selectedCategory.categoryId
+        ? 0
+        : selectedCategory.categoryId;
+    setSelectedCategory(newCategoryId);
+  };
 
   return (
     <div className="home-wrapper">
-      <Container title="Kategoriler" titleIcon={<CategoryIcon/>} titleClass="category-title" classes="category-content">
-        {renderCategories()}
+      <Container
+        title="Kategoriler"
+        titleIcon={<CategoryIcon />}
+        titleClass="category-title"
+        classes="category-content"
+      >
+        <Button
+          classes={`category-button ${categoryId === 0 && "active"}`}
+          clickHandler={() => changeCategory(0)}
+        >
+          Tüm Kategoriler
+        </Button>
+        {categories.map((el, index) => (
+          <Button
+            key={el.categoryId}
+            classes={`category-button ${
+              categoryId === el.categoryId && "active"
+            }`}
+            clickHandler={() => changeCategory(index + 1)}
+          >
+            {el.categoryName}
+          </Button>
+        ))}
       </Container>
     </div>
   );
